b864f1339410a5a07381927ac0eeaee77e4147c0
Sudip ghosh <49285187+imsudip@users.noreply.github.com>
2021-02-25 15:53:56 +0530

    Revert "ch"
    This reverts commit 6dce49541b5ee57525684d903470da55721b3f18.

--- lib/style.dart
+++ lib/style.dart
@@ -7,20 +7,16 @@ TextStyle headline1 =
     GoogleFonts.hammersmithOne(fontSize: 28, fontWeight: FontWeight.bold);
 TextStyle pageTitle =
     GoogleFonts.poppins(fontSize: 28, fontWeight: FontWeight.w500);
-TextStyle button =
-    GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.w500);
-TextStyle money =
-    GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.w600);
 TextStyle subtitle1 =
-    GoogleFonts.lato(fontSize: 16, fontWeight: FontWeight.w500, height: 1.2);
+    GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.w400);
 TextStyle subtitle2 =
     GoogleFonts.poppins(fontSize: 14, fontWeight: FontWeight.w400);
 TextStyle bodytext2 =
     GoogleFonts.poppins(fontSize: 12, fontWeight: FontWeight.w400);
 Color red = Color(0xffe63946);
 Color blue = Color(0xff39A0ED);
-Color white = Color(0xffF6F6F6);
+Color white = Color(0xffF9F5FF);
 Color blueDark = Color(0xff0B132B);
 Color black = Color(0XFF0D1317);
-Color grey = Color(0xffC0C0C0);
+Color grey = Color(0xff4A525A);
 Color greyLight = Color(0xffD8D8D8);
